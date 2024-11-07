8686e8950d3424afb3c81e45a6f15d5819f99233
dPoz <pozniakdp@outlook.com>
2024-11-06 18:37:40 -0700

    small fixes

--- mysql/VacationPlannerTableSetup - TestScript.sql
+++ mysql/VacationPlannerTableSetup - TestScript.sql
@@ -9,6 +9,8 @@ call AddUser("test", "testpassword");
 
 call AddUser("a", "a");
 
+call AddUser("Neos", "this-is-garbage8");
+
 SELECT * FROM users;
 
 call AddQueries('Poz', '2024-10-30', '2024-11-5',  'Calgary', 'All You Can Eats', 'Mexico', 1000, true, false, false, false);
