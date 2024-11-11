9990eb0aafa612cbea8bd45efdb747988ef37942
dPoz <pozniakdp@outlook.com>
2024-11-11 14:23:45 -0700

    cleaned up unused functions in database.py

--- database.py
+++ database.py
@@ -5,9 +5,6 @@ from sqlalchemy import create_engine, text
 
 load_dotenv()
 
-# Create a connection to the database
-
-
 class database:
     def __init__(self):
         # Read the connection string components from the .env file
@@ -18,7 +15,6 @@ class database:
         dbname = os.getenv('DB_NAME')
 
         # Format the connection string
-        #
         connection_string = f'mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}?charset=utf8mb4'
 
         # Create the engine using the connection string
@@ -29,15 +25,6 @@ class database:
             with self.engine.connect() as connection:
                 result = connection.execute(text(query_string))
                 return result
-        except:
-            return False
-
-    def insert(self, insert_string):
-        try:
-            with self.engine.connect() as connection:
-                connection.execute(text(insert_string))
-                connection.commit()
-                return True
         except:
             return False
             
@@ -56,39 +43,6 @@ class database:
                     connection.execute(text(insert_string).bindparams(**params))
         except Exception as e:
             print(f"Error: {e}")
-
-    def delete(self, delete_string):
-        try:
-            with self.engine.connect() as connection:
-                connection.execute(text(delete_string))
-                connection.commit()
-                return True
-        except:
-            return False
-
-    def update(self, update_string):
-        try:
-            with self.engine.connect() as connection:
-                connection.execute(text(update_string))
-                connection.commit()
-                return True
-        except:
-            return False
-
-    def add_user(self, name, password):
-        with self.engine.connect() as connection:
-            # Create a text object with the CALL statement
-            call_procedure = text("CALL AddUser(:username, :password)")
-            input = {"username": name, "password": password}
-            # Execute the procedure with parameters
-            result = connection.execute(call_procedure, input)
-
-            # If the procedure returns any results, you can fetch them like this:
-            # for row in result:
-            #     print(row)
-
-            # Commit the transaction
-            connection.commit()
 
     def load_queries_dicts_from_db(self, user_id):
         result = self.query(f"select * from queries where userId = {user_id}")
@@ -121,8 +75,3 @@ class database:
                             }
             responses.append(response_dic)
         return responses
-
-# db1 = database()
-
-# queries = db1.load_queries_dicts_from_db()
-# print(queries)
